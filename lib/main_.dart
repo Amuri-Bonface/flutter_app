fa20a22f179bff2132c2cb1ac72def078e3a4676
Amuri Bonface <amuribonface@gmail.com>
2018-10-23 17:26:39 +0300

    using lists,image,and button-onpress

--- lib/main_.dart
+++ lib/main_.dart
@@ -5,12 +5,14 @@ import 'package:flutter_app/add_flavor/random_ness.dart';
 
 class MyAppy extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return _MyApp();
-    }}
-class _MyApp extends State<MyAppy>{
-List<String> _product=["ova"];
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyAppy> {
+  List<String> _product = ["ova"];
   @override
   Widget build(BuildContext context) {
     //  final wordPair=WordPair.random();
@@ -25,24 +27,34 @@ List<String> _product=["ova"];
           children: <Widget>[
             Container(
               child: RaisedButton(
-                
                 child: Text("data"),
-                onPressed: () {},
-                
+                onPressed: () {
+                  setState(() {
+                                     _product.add("Advanced Stuff"); 
+                                    });
+                },
               ),
-              margin: EdgeInsets.all(0.0),
-              width: 100.0,
+              margin: EdgeInsets.all(10.0),
+             // width: 100.0,
             ),
-            Column(children: <Widget>[Card(
-              child: Column(
-                children: <Widget>[Image.asset("assets/a.jpg"), Text("Element")],
-              ),
-              margin: EdgeInsets.all(30.0),
-              // child: Text(wordPair.asPascalCase),
-              //child: RandomWords(),
-            )],)
+            Column(
+              children: 
+                _product.map((element)=>Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset("assets/a.jpg"),
+                      Text(element)
+                    ],
+                  ),
+                  margin: EdgeInsets.all(30.0),
+                  // child: Text(wordPair.asPascalCase),
+                  //child: RandomWords(),
+                )
+                ).toList()
+                             ,
+            )
 
-             //child: RandomWords(),
+            //child: RandomWords(),
           ],
         ),
         floatingActionButton: new FloatingActionButton(
